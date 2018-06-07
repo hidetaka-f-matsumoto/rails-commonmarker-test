d62fb133f09cc288f903a66249158448eaf174cd
Hidetaka Matsumoto <tanemomi06@gmail.com>
2018-06-07 22:33:29 +0900

    scaffold documents

---
+++ db/migrate/20180607132401_create_documents.rb
@@ -0,0 +1,11 @@
+class CreateDocuments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :documents do |t|
+      t.text :title
+      t.text :markdown
+      t.text :html
+
+      t.timestamps
+    end
+  end
+end
