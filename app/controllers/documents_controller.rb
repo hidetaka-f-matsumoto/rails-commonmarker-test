74d2be6a431fc607984838e8a8bec5024b2a0cc9
Hidetaka Matsumoto <tanemomi06@gmail.com>
2018-06-15 18:55:02 +0900

    Remove action for json format.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,83 +1,57 @@
 class DocumentsController < ApplicationController
   before_action :set_document, only: [:show, :edit, :update, :destroy]
 
-  # GET /documents
-  # GET /documents.json
   def index
     @documents = Document.order(id: :desc)
   end
 
-  # GET /documents/1
-  # GET /documents/1.json
   def show
   end
 
-  # GET /documents/new
   def new
     @document = Document.new
   end
 
-  # GET /documents/1/edit
   def edit
   end
 
-  # POST /documents
-  # POST /documents.json
   def create
     @document = Document.new(document_params)
 
-    respond_to do |format|
-      if params[:preview_button]
-        @document.assign_html
-        format.html { render :new }
-        format.json { render :new }
-      elsif @document.save
-        format.html { redirect_to @document, notice: 'Document was successfully created.' }
-        format.json { render :show, status: :created, location: @document }
-      else
-        format.html { render :new }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
-      end
+    if params[:preview_button]
+      @document.assign_html
+      render :new
+    elsif @document.save
+      redirect_to @document, notice: 'Document was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /documents/1
-  # PATCH/PUT /documents/1.json
   def update
-    respond_to do |format|
-      if params[:preview_button]
-        @document.assign_attributes(document_params)
-        @document.assign_html
-        format.html { render :edit }
-        format.json { render :edit }
-      elsif @document.update(document_params)
-        format.html { redirect_to @document, notice: 'Document was successfully updated.' }
-        format.json { render :show, status: :ok, location: @document }
-      else
-        format.html { render :edit }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
-      end
+    if params[:preview_button]
+      @document.assign_attributes(document_params)
+      @document.assign_html
+      render :edit
+    elsif @document.update(document_params)
+      redirect_to @document, notice: 'Document was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /documents/1
-  # DELETE /documents/1.json
   def destroy
     @document.destroy
-    respond_to do |format|
-      format.html { redirect_to documents_url, notice: 'Document was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to documents_url, notice: 'Document was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_document
-      @document = Document.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def document_params
-      params.fetch(:document, {}).permit(:title, :markdown)
-    end
+  def set_document
+    @document = Document.find(params[:id])
+  end
+
+  def document_params
+    params.fetch(:document, {}).permit(:title, :markdown)
+  end
 end
